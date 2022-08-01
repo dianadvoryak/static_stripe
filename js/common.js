cc96dfdd32834c11ae504e8015beeb4c1cce24da
Diana Dvoryak <dianadvoryaknotifications@gmail.com>
2022-08-01 11:11:05 +0200

    Modify js

--- js/common.js
+++ js/common.js
@@ -1,8 +1,9 @@
 $(function() {
-  $('.header__burger').click(function(event){
+  	$('.header__burger').click(function(event){
 		$('.header__burger, .menu').toggleClass('active');
-		$('body').toggleClass('lock');
-
+	});
+	$('.menu__link').click(function(event){
+		$('.header__burger, .menu').toggleClass('active');
 	});
 
 });
